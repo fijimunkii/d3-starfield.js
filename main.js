16d7c353b5842c2bf0edd03d8d8d8832c2083a3b
Harrison Powers <fijimunkii@gmail.com>
2014-02-13 18:06:33 -0500

    cant scroll

--- main.js
+++ main.js
@@ -2,138 +2,113 @@
     "use strict";
  
     var width, height;
+    
     function getSize(){
-        width = w.innerWidth,
-        height = w.innerHeight;
+      width = w.innerWidth,
+      height = w.innerHeight;
  
-        if(width === 0 || height === 0){
-            setTimeout(function(){
-                getSize();
-            }, 100);
-        }
-        else {
-            init();
-        }
+      if(width === 0 || height === 0){
+        setTimeout(function(){
+          getSize();
+        }, 100);
+      } else {
+        init({frame: 1});
+        init({frame: 2});
+      }
     }
  
-    function init(){
- 
-        //Setup path for outerspace
-        var space = d3.geo.azimuthal()
-            .mode("equidistant")
-            .translate([width / 2, height / 2]);
- 
-        space.scale(space.scale() * 3);
- 
-        var spacePath = d3.geo.path()
-            .projection(space)
-            .pointRadius(1);
- 
-        //Setup path for globe
-        var projection = d3.geo.azimuthal()
-            .mode("orthographic")
-            .translate([width / 2, height / 2]);
- 
-        var scale0 = projection.scale();
- 
-        var path = d3.geo.path()
-            .projection(projection)
-            .pointRadius(2);
- 
-        //Setup zoom behavior
-        var zoom = d3.behavior.zoom(true)
-            .translate(projection.origin())
-            .scale(projection.scale())
-            .scaleExtent([100, 800])
-            .on("zoom", move);
- 
-        var circle = d3.geo.greatCircle();
- 
-        var svg = d3.select("body")
-            .append("svg")
-                .attr("width", width)
-                .attr("height", height)
-                .append("g");
- 
-        //Create a list of random stars and add them to outerspace
-        var starList = createStars(300);
-                
-        var stars = svg.append("g")
-            .selectAll("g")
-            .data(starList)
-            .enter()
-            .append("path")
-                .attr("class", "star")
-                .attr("d", function(d){
-                    spacePath.pointRadius(d.properties.radius);
-                    return spacePath(d);
-                });
- 
- 
-        svg.append("rect")
-            .attr("class", "frame")
-            .attr("width", width)
-            .attr("height", height);
- 
-        var g = svg.append("g"),
-            features;
+    function init(options){
 
-        //Redraw all items with new projections
-        function redraw(){
-            features.attr("d", function(d){
-                return path(circle.clip(d));
+      //Setup path for outerspace
+      var space = d3.geo.azimuthal()
+          .mode("equidistant")
+          .translate([width / 2, height / 2]);
+
+      space.scale(space.scale() * 3);
+
+      var spacePath = d3.geo.path()
+          .projection(space)
+          .pointRadius(1);
+
+      //Setup path for globe
+      var projection = d3.geo.azimuthal()
+          .mode("orthographic")
+          .translate([width / 2, height / 2]);
+
+      var scale0 = projection.scale();
+
+      var path = d3.geo.path()
+          .projection(projection)
+          .pointRadius(2);
+
+      var circle = d3.geo.greatCircle();
+
+      var svg = d3.select("body")
+          .append("svg")
+          .attr("width", width)
+          .attr("height", height)
+          .append("g");
+
+      //Create a list of random stars and add them to outerspace
+      var starList = createStars({number: 2000, random: true});
+              
+      var stars = svg.append("g")
+          .selectAll("g")
+          .data(starList)
+          .enter()
+          .append("path")
+            .attr("class", "star")
+            .attr("d", function(d){
+              spacePath.pointRadius(d.properties.radius);
+              return spacePath(d);
             });
- 
-            stars.attr("d", function(d){
-                spacePath.pointRadius(d.properties.radius);
-                return spacePath(d);
+
+
+      svg.append("rect")
+          .attr("class", "frame" + options.frame)
+          .attr("width", width)
+          .attr("height", height);
+
+      var g = svg.append("g"),
+          features;
+
+
+      function createStars(options){
+        var data = [];
+          
+        for(var i = 0; i < options.number; i++){
+          if (options.random) {
+            data.push({
+              geometry: {
+                type: 'Point',
+                coordinates: randomLonLat()
+              },
+              type: 'Feature',
+              properties: {
+                radius: Math.random()
+              }
             });
-        }
- 
- 
-        function move() {
-            if(d3.event){
-                var scale = d3.event.scale;
-                var origin = [d3.event.translate[0] * -1, d3.event.translate[1]];
-                
-                projection.scale(scale);
-                space.scale(scale * 3);
-                backgroundCircle.attr('r', scale);
-                path.pointRadius(2 * scale / scale0);
- 
-                projection.origin(origin);
-                circle.origin(origin);
-                
-                //globe and stars spin in the opposite direction because of the projection mode
-                var spaceOrigin = [origin[0] * -1, origin[1] * -1];
-                space.origin(spaceOrigin);
-                redraw();
-            }
-        }
- 
- 
-        function createStars(number){
-            var data = [];
-            for(var i = 0; i < number; i++){
-                data.push({
-                    geometry: {
-                        type: 'Point',
-                        coordinates: randomLonLat()
-                    },
-                    type: 'Feature',
-                    properties: {
-                        radius: Math.random() * 1.5
-                    }
-                });
-            }
-            return data;
-        }
- 
-        function randomLonLat(){
-            return [Math.random() * 360 - 180, Math.random() * 180 - 90];
-        }
-    }
- 
-    getSize();
- 
+          } else {
+            data.push({
+              geometry: {
+                type: 'Point',
+                coordinates: randomLonLat()
+              },
+              type: 'Feature',
+              properties: {
+                radius: 1.5
+              }
+            });
+          }
+        }    
+        return data;
+      }
+
+      function randomLonLat(){
+          return [Math.random() * 360 - 180, Math.random() * 180 - 90];
+      }
+  }
+
+  getSize();
+
 }(window, d3));
